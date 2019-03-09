10fdefc949e7248cfc3a7119c4b7070eedffa794
Alexis Vergnet <a..v.vergnet@gmail.com>
2019-03-09 13:23:55 -0800

    Assignment 5 commit

---
+++ app/src/main/java/com/example/extstudent/assignment5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.extstudent.assignment5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.listView = (ListView)findViewById(R.id.listView);
+
+        initListView();
+    }
+    private void initListView(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_layout_entry);
+
+        adapter.add("Object 1");
+        adapter.add("Object 2");
+
+        String[] myObjects = {"1", "2"};
+        adapter.addAll(myObjects);
+
+        this.listView.setAdapter(adapter);
+    }
+}
